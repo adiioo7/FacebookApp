5454b9ea0d122ea98ddea1d82da4e0b9e292b14b
unknown <Home_7@Home_7-PC.(none)>
2012-07-02 22:26:28 +0530

    Added base files

---
+++ ContentPage/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Facebook;
+using Facebook.Web;
+
+
+public partial class ContentPage_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Facebook.Web.FacebookWebContext objFacebook = new Facebook.Web.FacebookWebContext(
+        
+    }
+}
